9bfac3d1c25f253c93f0a31a59cbc52ea7bccac4
Lemon <ervinjayceetiu@gmail.com>
2024-12-18 04:39:24 +0800

    first commit

---
+++ scripts/notifications.js
@@ -0,0 +1,33 @@
+if(Notification.permission !== 'granted') {
+    Notification.requestPermission()
+        .then(perm => {
+            if(perm === 'granted') {
+                startNotifs()
+            }
+        })
+}
+
+if(Notification.permission === 'granted') {
+    startNotifs()
+}
+
+function startNotifs() {
+    setInterval(() => {
+        new Notification('A contestant has died!', {
+            body: 'Recover them using MeLife!'
+        })
+    }, 10000)
+}
+
+const popup = document.createElement('dialog')
+popup.innerText = 'This website is not completed! This website is only for desktop.'
+popup.style.color = 'black'
+popup.style.borderRadius = '10px'
+popup.style.border = '2px solid black'
+
+document.body.appendChild(popup)
+popup.showModal()
+
+setTimeout(() => {
+    document.body.removeChild(popup)
+}, 2000)
